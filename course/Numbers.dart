367f0db2e49bf025e308c8b96c1306f3a0a85fcd
Tarek994 <ghali.tarek.123@hotmail.com>
2024-01-05 19:32:38 +0200

    how to use parsing

--- course/Numbers.dart
+++ course/Numbers.dart
@@ -1,12 +1,17 @@
 // Numbers
 
 main () {
- // integers
-  int age = 23;
-  print(age);
+//  // integers
+//   int age = 23;
+//   print(age);
 
-// doubles
-double note = 16.5;
-print(note);
+// // doubles
+// double note = 16.5;
+// print(note);
+
+/////// parsing to bring a number from string 
+if ('10' == num.parse('10')) {
+  print('ok');
+}
 
 }
