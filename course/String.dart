dd84e8877f27e7d8a5f92e56a883d49bec0c4fd1
Tarek994 <ghali.tarek.123@hotmail.com>
2024-01-30 23:55:02 +0200

    string: how to display strings vars

--- course/String.dart
+++ course/String.dart
@@ -2,9 +2,21 @@
 
 
 main () {
+
+  // Syntax
+
   String name = 'algorithm';
   String name1 = 'Algorithm';
   String lines = ''''line1
   line2
   ''';
+  String lines1 = """line3
+  line4
+ """;
+
+  print(name);
+  print(name1);
+  print(lines);
+  print(lines1);
+
 }
